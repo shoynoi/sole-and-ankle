2f906f88bd26ff4e651373ab6be236ea63ef6665
shoynoi <shoynoi.jp@gmail.com>
2021-11-16 20:46:47 +0900

    Exercise 2 solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <Side>
+          <Logo />
+        </Side>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,24 +23,29 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Side />
       </MainHeader>
     </header>
   );
 };
 
 const MainHeader = styled.div`
-  padding: 26px 32px;
+  padding: 18px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   display: flex;
-  align-items: center;
+  align-items: baseline;
+  height: 72px;
 `;
 
 const Nav = styled.nav`
   display: flex;
-  justify-content: center;
-  flex: 1;
   gap: 48px;
+  margin: 0 48px;
 `;
+
+const Side = styled.div`
+  flex: 1;
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
