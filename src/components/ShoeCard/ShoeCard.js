9576ba1ad76f1c38a50bc7a85eb3e57187df78bf
shoynoi <shoynoi.jp@gmail.com>
2021-11-17 21:21:24 +0900

    Exercise 4A

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,18 +53,28 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex-grow: 1;
+  flex-basis: 340px;
+  //max-width: 340px;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -14,6 +14,11 @@ const ShoeGrid = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  //justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 64px 32px;
+`;
 
 export default ShoeGrid;
